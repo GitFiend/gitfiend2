006788f8b14a8a7603760e85ac1b6c92698f065d
Toby <sugto555@gmail.com>
2024-05-26 18:22:21 +1200

    Get config parser working.

--- core/git/parseconfig.go
+++ core/git/parseconfig.go
@@ -1,30 +1,80 @@
 package git
 
-import . "gitfiend2/core/parser"
+import (
+	. "gitfiend2/core/parser"
+	"strings"
+)
+
+type Row interface {
+	String() string
+	Data() bool
+}
+
+type Section struct {
+	Heading Heading
+	Rows    []Row
+}
+
+func (s Section) String() string {
+	var res string
+	heading := s.Heading.String()
+
+	for _, row := range s.Rows {
+		if row.Data() {
+			res += heading + "." + row.String() + "\n"
+		}
+	}
+	return strings.Trim(res, "\n ")
+}
+func (s Section) Data() bool {
+	return true
+}
 
 type Heading [2]string
 
-type Row [2]string
-
-type Other struct {
-	Kind  OtherKind
-	Value string
+func (h Heading) String() string {
+	if h[1] == "" {
+		return h[0]
+	} else {
+		return h[0] + "." + h[1]
+	}
 }
 
-type OtherKind string
+type DataRow [2]string
 
-const (
-	Comment OtherKind = "comment"
-	Unknown           = "unknown"
-	Valid             = "valid"
-)
+func (r DataRow) String() string {
+	return r[0] + "=" + r[1]
+}
+func (r DataRow) Data() bool {
+	if r[0] == "" || r[1] == "" {
+		return false
+	}
+	return true
+}
+
+type Unknown string
+type Comment string
+
+func (c Comment) String() string {
+	return string(c)
+}
+func (c Comment) Data() bool {
+	return false
+}
+func (o Unknown) String() string {
+	return string(o)
+}
+func (o Unknown) Data() bool {
+	return false
+}
+
+var pConfig = Many(Or(pSection, pOther))
+
+var pHeading = Or(pHeading1, pHeading2)
 
 var pHeading1 = Map(And3(Rune('['), AnyWord, Rune(']')),
 	func(result And3Result[rune, string, rune]) Heading {
-		return Heading{
-			result.R2,
-			"",
-		}
+		return Heading{result.R2}
 	})
 
 var pHeading2 = Map(And5(Rune('['), AnyWord, Ws, StringLiteral, Rune(']')),
@@ -32,25 +82,39 @@ var pHeading2 = Map(And5(Rune('['), AnyWord, Ws, StringLiteral, Rune(']')),
 		return Heading{res.R2, res.R4}
 	})
 
-var pHeading = Or(pHeading1, pHeading2)
-
 var pRow = Map(And6(Ws, AnyWord, Ws, Rune('='), Ws, UntilLineEnd),
 	func(res And6Result[string, string, string, rune, string, string]) Row {
-		return Row{res.R2, res.R6}
+		return DataRow{res.R2, res.R6}
+	})
+
+var pSection = Map(And2(pHeading, Many(Or(pRow, pComment, pUnknown))),
+	func(res And2Result[Heading, []Row]) Row {
+		return Section{res.R1, res.R2}
 	})
 
 var pComment = Map(And3(Ws, Or(Rune(';'), Rune('#')), UntilLineEnd),
-	func(res And3Result[string, rune, string]) Other {
-		return Other{
-			Kind:  Comment,
-			Value: res.R3,
-		}
+	func(res And3Result[string, rune, string]) Row {
+		return Comment(res.R3)
 	})
 
 var pUnknown = Map(And3(Not(pHeading), Not(pRow), UntilLineEnd),
-	func(res And3Result[bool, bool, string]) Other {
-		return Other{}
+	func(res And3Result[bool, bool, string]) Row {
+		return Unknown(res.R3)
 	})
 
-//var pOther = Or()
-//var pRowOther = pOther
+var pOther = Or(pComment, pUnknown)
+
+func MakeConfigLog(text string) (string, bool) {
+	res, ok := ParseAll(pConfig, text)
+
+	if ok {
+		var log string
+		for _, row := range res {
+			if row.Data() {
+				log += row.String() + "\n"
+			}
+		}
+		return strings.Trim(log, "\n "), true
+	}
+	return "", false
+}
--- core/git/parseconfig_test.go
+++ core/git/parseconfig_test.go
@@ -1,1 +1,138 @@
 package git
+
+import (
+	"gitfiend2/core/parser"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPHeading(t *testing.T) {
+	res, ok := parser.ParseAll(pHeading, "[core]")
+	assert.True(t, ok)
+	assert.Equal(t, "core", res.String())
+
+	res, ok = parser.ParseAll(pHeading, "[remote \"origin\"]")
+	assert.True(t, ok)
+	assert.Equal(t, "remote.origin", res.String())
+
+	res, ok = parser.ParseAll(pHeading, "[branch \"my-branch-name\"]")
+	assert.True(t, ok)
+	assert.Equal(t, "branch.my-branch-name", res.String())
+
+	res, ok = parser.ParseAll(pHeading, "[branch \"feature/my-branch-name\"]")
+	assert.True(t, ok)
+	assert.Equal(t, "branch.feature/my-branch-name", res.String())
+}
+
+func TestParseSimpleConfig(t *testing.T) {
+	text := `
+[core]
+	repositoryformatversion = 0
+	filemode = true 
+`
+
+	res, ok := MakeConfigLog(text)
+	assert.True(t, ok)
+	assert.Equal(t, "core.repositoryformatversion=0\ncore.filemode=true", res)
+}
+
+func TestParseRandomComments(t *testing.T) {
+	text := `
+; Comment
+[core]
+	repositoryformatversion = 0
+	filemode = true 
+# hello
+`
+	res, ok := MakeConfigLog(text)
+	assert.True(t, ok)
+	assert.Equal(t, "core.repositoryformatversion=0\ncore.filemode=true", res)
+}
+
+func TestParseRealConfig(t *testing.T) {
+	text := `[core]
+	repositoryformatversion = 0
+	filemode = true
+	bare = false
+	logallrefupdates = true
+[remote "origin"]
+	url = https://github.com/GitFiend/gitfiend-core.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "main"]
+	remote = origin
+	merge = refs/heads/main
+[branch "ssr-code-viewer"]
+	remote = origin
+	merge = refs/heads/ssr-code-viewer
+`
+	res, ok := MakeConfigLog(text)
+	assert.True(t, ok)
+	assert.Equal(t, `core.repositoryformatversion=0
+core.filemode=true
+core.bare=false
+core.logallrefupdates=true
+remote.origin.url=https://github.com/GitFiend/gitfiend-core.git
+remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+branch.main.remote=origin
+branch.main.merge=refs/heads/main
+branch.ssr-code-viewer.remote=origin
+branch.ssr-code-viewer.merge=refs/heads/ssr-code-viewer`, res)
+}
+
+func TestParseLargerConfig(t *testing.T) {
+	text := `[core]
+	repositoryformatversion = 0
+	filemode = true
+	bare = false
+	logallrefupdates = true
+	ignorecase = true
+	precomposeunicode = true
+# Some comment.
+[remote "origin"]
+	url = https://github.com/GitFiend/git-fiend.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+
+; Some comment 2.
+[branch "main"]
+	remote = origin
+	merge = refs/heads/main
+[branch "cleanup"]
+	remote = origin
+	merge = refs/heads/cleanup
+[branch "commit-switcher"]
+	remote = origin
+	merge = refs/heads/commit-switcher
+[branch "server"]
+	remote = origin
+	merge = refs/heads/server
+[branch "ws"]
+	remote = origin
+	merge = refs/heads/ws
+[branch "alt-toolbar"]
+	remote = origin
+	merge = refs/heads/alt-toolbar
+[branch "alt-ref-view"]
+	remote = origin
+	merge = refs/heads/alt-ref-view
+[branch "image-conflicts"]
+	remote = origin
+	merge = refs/heads/image-conflicts
+[branch "auto-complete"]
+	remote = origin
+	merge = refs/heads/auto-complete
+[branch "mac-app"]
+	remote = origin
+	merge = refs/heads/mac-app
+[branch "try-tauri"]
+	remote = origin
+	merge = refs/heads/try-tauri
+[branch "split-view"]
+	remote = origin
+	merge = refs/heads/split-view
+[branch "ssr-code-viewer"]
+	remote = origin
+	merge = refs/heads/ssr-code-viewer
+`
+	_, ok := MakeConfigLog(text)
+	assert.True(t, ok)
+}
