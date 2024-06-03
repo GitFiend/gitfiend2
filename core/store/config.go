5de8e6ea43bcbd6f70ec98a7cc2ffabdf6772942
Toby <sugto555@gmail.com>
2024-06-03 16:28:31 +1200

    Work to read git config.

---
+++ core/store/config.go
@@ -0,0 +1,45 @@
+package store
+
+import (
+	"fmt"
+	"gitfiend2/core/git"
+	"os"
+	"path"
+)
+
+type GitConfig struct {
+	Entries    map[string]string
+	Remotes    map[string]string
+	Submodules map[string]string
+}
+
+func LoadConfigFromDisk(repoPath string) (GitConfig, error) {
+	repo, ok := GetRepoPath(repoPath)
+	if ok {
+		return GitConfig{}, fmt.Errorf("couldn't load config for %s", repoPath)
+	}
+
+	configPath := path.Join(repo.GitPath, "config")
+
+	bytes, err := os.ReadFile(configPath)
+	if err != nil {
+		return GitConfig{}, err
+	}
+
+	rows, ok := git.ParseConfig(string(bytes))
+	if !ok {
+		return GitConfig{}, fmt.Errorf("failed to parse config %s", configPath)
+	}
+
+	for _, row := range rows {
+		switch r := row.(type) {
+		case git.Section:
+			if r.Heading[0] == "remote" {
+				//
+			}
+		case git.DataRow:
+		}
+	}
+
+	return GitConfig{}, nil
+}
