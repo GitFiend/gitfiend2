f1a714a094b2787b44313d9a973642278817f088
Toby Suggate <tjsuggate@gmail.com>
2024-09-06 15:43:41 +1200

    Write parsers for patches.

--- core/git/parsepatches.go
+++ core/git/parsepatches.go
@@ -1,19 +1,85 @@
 package git
 
-import p "gitfiend2/core/parser"
+import (
+	"fmt"
+	p "gitfiend2/core/parser"
+)
 
-//var pPatchesWithCommitId = p.And3(
-//	p.UntilParser(p.And2(p.Rune(','), p.Ws)),
-//	)
+var pManyPatchesWithCommitIds = p.Many(pPatchesWithCommitId)
+
+var pPatchesWithCommitId = p.And3(
+	p.UntilParser(p.And2(p.Rune(','), p.Ws)),
+	pPatches,
+	p.Or(p.UntilNul, p.Ws),
+)
 
 type patchData struct {
 	id, oldFile, newFile string
 	patchType            PatchType
 }
 
-var pRenamePatch = p.And4(
-	p.And2(p.Rune('R'), p.UInt),
-	p.UntilNul,
-	p.UntilNul,
-	p.UntilNul,
+var pPatches = p.Many(p.Or(pRenamePatch, pCopyPatch, pOtherPatch))
+
+var pRenamePatch = p.Map(
+	p.And4(
+		p.And2(p.Rune('R'), p.UInt),
+		p.UntilNul,
+		p.UntilNul,
+		p.UntilNul,
+	),
+	makePatchData,
 )
+
+var pCopyPatch = p.Map(
+	p.And4(
+		p.And2(p.Rune('C'), p.UInt),
+		p.UntilNul,
+		p.UntilNul,
+		p.UntilNul,
+	),
+	makePatchData,
+)
+
+func makePatchData(
+	res p.And4Result[
+		p.And2Result[rune, string],
+		string,
+		string,
+		string],
+) patchData {
+	return patchData{
+		id:        fmt.Sprintf("%s-%c%s", res.R4, res.R1.R1, res.R1.R2),
+		patchType: PatchType(res.R1.R1),
+		oldFile:   res.R3,
+		newFile:   res.R4,
+	}
+}
+
+var pStatus = p.Map(
+	p.Or(
+		p.Rune('A'),
+		p.Rune('B'),
+		p.Rune('C'),
+		p.Rune('D'),
+		p.Rune('M'),
+		p.Rune('T'),
+		p.Rune('U'),
+		p.Rune('X'),
+	), func(res rune) PatchType {
+		return PatchType(res)
+	},
+)
+
+var pOtherPatch = p.Map(
+	p.And3(pStatus, p.UntilNul, p.UntilNul),
+	makeFromOther,
+)
+
+func makeFromOther(res p.And3Result[PatchType, string, string]) patchData {
+	return patchData{
+		id:        fmt.Sprintf("%s-%c", res.R3, res.R1),
+		patchType: res.R1,
+		oldFile:   res.R3,
+		newFile:   res.R3,
+	}
+}
---
+++ core/git/parsepatches_test.go
@@ -0,0 +1,56 @@
+package git
+
+import (
+	"fmt"
+	"gitfiend2/core/parser"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const p1 = "src2/parser-lib/input.ts"
+const p2 = "src2/renderer-process/redux-store/repo-state/commits/commits-reducer.test.ts"
+
+func TestPRenamePatch(t *testing.T) {
+	log := fmt.Sprintf("R100\000%s\000%s\000", p1, p2)
+	res, ok := parser.ParseAll(pRenamePatch, log)
+
+	assert.True(t, ok)
+	assert.Equal(
+		t, patchData{
+			id:        fmt.Sprintf("%s-R100", p2),
+			oldFile:   p1,
+			newFile:   p2,
+			patchType: 'R',
+		}, res,
+	)
+}
+
+func TestPCopyPatch(t *testing.T) {
+	log := fmt.Sprintf("C100\000%s\000%s\000", p1, p2)
+	res, ok := parser.ParseAll(pCopyPatch, log)
+
+	assert.True(t, ok)
+	assert.Equal(
+		t, patchData{
+			id:        fmt.Sprintf("%s-C100", p2),
+			oldFile:   p1,
+			newFile:   p2,
+			patchType: 'C',
+		}, res,
+	)
+}
+
+func TestPOtherPatch(t *testing.T) {
+	log := fmt.Sprintf("M\000%s\000", p2)
+	res, ok := parser.ParseAll(pOtherPatch, log)
+
+	assert.True(t, ok)
+	assert.Equal(
+		t, patchData{
+			id:        fmt.Sprintf("%s-M", p2),
+			oldFile:   p2,
+			newFile:   p2,
+			patchType: 'M',
+		}, res,
+	)
+}
